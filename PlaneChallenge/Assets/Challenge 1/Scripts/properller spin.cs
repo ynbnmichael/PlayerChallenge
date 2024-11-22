81538ccbb136855a39d7826f702496335e6603ea
easdmichael <kinaram@roverkids.org>
2024-11-22 13:33:07 -0500

    propeller spin complete

--- PlaneChallenge/Assets/Challenge 1/Scripts/properller spin.cs
+++ PlaneChallenge/Assets/Challenge 1/Scripts/properller spin.cs
@@ -1,16 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class properllerspin : MonoBehaviour
+public class SpinPropellerX : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float propellerSpeed = 1000;
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward, propellerSpeed * Time.deltaTime);
     }
 }
